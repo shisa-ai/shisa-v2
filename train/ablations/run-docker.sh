e9e6cd28b900f2e9c595be24edfd7614be1df4db
lhl <lhl@randomfoo.net>
2024-07-28 14:23:03 +0900

    updated axolotl docker script

--- train/ablations/run-docker.sh
+++ train/ablations/run-docker.sh
@@ -1,3 +1,4 @@
-# docker run --gpus '"all"' --rm -it winglian/axolotl:main-latest
-docker run --privileged --gpus '"all"' --shm-size 10g --rm -it --name axolotl --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 --mount type=bind,src="${PWD}",target
-=/workspace/axolotl -v ${HOME}/.cache/huggingface:/root/.cache/huggingface winglian/axolotl:main-latest
+#!/usr/bin/bash
+
+# sudo docker run --gpus '"all"' --rm -it winglian/axolotl:main-latest
+sudo docker run --privileged --gpus '"all"' --shm-size 10g --rm -it --name axolotl --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 --mount type=bind,src="${PWD}",target=/workspace/axolotl -v ${HOME}/.cache/huggingface:/root/.cache/huggingface winglian/axolotl:main-latest
