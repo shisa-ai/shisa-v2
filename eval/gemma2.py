8eab8a0e0322602d7a44404fed679691965d23cc
lhl <lhl@randomfoo.net>
2024-07-30 00:20:07 +0900

    gemma2 test

---
+++ eval/gemma2.py
@@ -0,0 +1,17 @@
+# pip install accelerate
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+
+tokenizer = AutoTokenizer.from_pretrained("google/gemma-2-9b-it")
+model = AutoModelForCausalLM.from_pretrained(
+    "google/gemma-2-9b-it",
+    device_map="auto",
+    torch_dtype=torch.bfloat16,
+)
+
+input_text = "Write me a poem about Machine Learning."
+input_ids = tokenizer(input_text, return_tensors="pt").to("cuda")
+
+outputs = model.generate(**input_ids)
+print(tokenizer.decode(outputs[0]))
+
