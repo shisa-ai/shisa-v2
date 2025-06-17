7a99f8488bb8f5df8618bd43d4c820d2f4f3988c
lhl <lhl@randomfoo.net>
2025-06-17 19:52:57 +0000

    new configs

---
+++ train/v2.1/014-smoothieqwen-8b-v2-dpo.openrlhf.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+export WANDB_ENTITY=augmxnt
+export WANDB_PROJECT="shisa-v2.1"           # keep your old project
+export HF_HUB_ENABLE_HF_TRANSFER=1
+export NCCL_ASYNC_ERROR_HANDLING=1
+
+MODEL="/data/outputs/012-smoothieqwen3-8b-v2-sft"    # or /data/outputs/012-smoothieqwen3-8b-v2-sft
+DATA="shisa-ai/shisa-v2-dpo-shuffled"
+OUT="/data/outputs/014-smoothieqwen3-8b-v2-dpo"
+
+deepspeed --num_gpus 4 --module openrlhf.cli.train_dpo \
+  --pretrain        "$MODEL" \
+  --dataset         "$DATA" \
+  --chosen_key      chosen \
+  --rejected_key    rejected \
+  --apply_chat_template \
+  --beta            0.1 \
+  --bf16 \
+  --zero_stage      3 \
+  --gradient_checkpointing \
+  --flash_attn \
+  --use_liger_kernel \
+  --train_batch_size       256 \
+  --micro_train_batch_size 8 \
+  --max_len         2048 \
+  --packing_samples \
+  --max_epochs      1 \
+  --learning_rate   5e-7 \
+  --lr_warmup_ratio 0.05 \
+  --save_steps      -1 \
+  --save_path       "$OUT" \
+  --logging_steps   1 \
+  --overlap_comm \
+  --use_wandb       014-smoothieqwen3-8b-v2-dpo
---
+++ train/v2.1/019-qwen3-30b-a3b-v2-sft.openrlhf.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+export WANDB_ENTITY=augmxnt
+export WANDB_PROJECT="shisa-v2.1"           # keep your old project
+export HF_HUB_ENABLE_HF_TRANSFER=1
+export NCCL_ASYNC_ERROR_HANDLING=1
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+MODEL="Qwen/Qwen3-30B-A3B"
+DATA="shisa-ai/shisa-v2-sft-shuffled"
+OUT="019-qwen3-30b-a3b-v2-sft"
+
+# GBS = 256 - LR = 5e-7 constant
+# GBS = 128 - LR = 2.5e-7
+# GBS = 64 - LR = 1.25e-7
+
+# --zero_stage      3 \
+# micro_train_batch_size 1
+# 97.92GiB - 69.7%
+# 95/90443 [58:00<895:21:51, 35.68s/it]
+
+deepspeed --num_gpus 8 --module openrlhf.cli.train_sft \
+  --pretrain        "$MODEL" \
+  --dataset         "$DATA" \
+  --input_key conversations \
+  --apply_chat_template \
+  --bf16 \
+  --zero_stage      3 \
+  --aux_loss_coef  0.001 \
+  --optimizer  paged_adamw_8bit \
+  --gradient_checkpointing \
+  --flash_attn \
+  --use_liger_kernel \
+  --train_batch_size       128 \
+  --micro_train_batch_size 16 \
+  --max_len 8192 \
+  --packing_samples \
+  --max_epochs      3 \
+  --learning_rate   1.63e-5 \
+  --lr_warmup_ratio 0.05 \
+  --save_steps      -1 \
+  --save_path       "$OUT" \
+  --logging_steps   1 \
+  --eval_steps -1 \
+  --eval_dataset "" \
+  --overlap_comm \
+  --use_wandb       True \
+  --wandb_org        augmxnt \
+  --wandb_project    shisa-v2.1 \
+  --wandb_run_name   "$OUT"
